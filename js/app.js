90a381afb026317fa0687ec0751813c1f4db6ed5
chrisborja <christianoborja@gmail.com>
2016-06-13 10:04:02 -0300

    Delete app.js

--- js/app.js
+++
@@ -1,8 +0,0 @@
-// var body = document.body;
-// body.style.color = "red";
-var a = document.createElement('a');
-var linkText = document.createTextNode("texto");
-a.appendChild(linkText);
-a.title = "link";
-a.href = "http://example.com";
-document.body.appendChild(a);
